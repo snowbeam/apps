51f8b2311c63d85f52432b96b9f5de12d8d5d6c2
Md Irshad <mdirshaddev+github@gmail.com>
2024-01-29 19:04:04 +0530

    chore: 🏗️ copied files, folder from previous work
    installed dependencies but nothing tested with build and dev commands
    for any of the application and packages code

---
+++ packages/components/workflow-editor/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "**/*.{ts,tsx,js,jsx,mdx}",
+    "**/*.stories.{ts,tsx,js,jsx,mdx}"
+  ],
+  darkMode: "media",
+  presets: [],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
---
+++ packages/modules/authentication/src/index.ts
@@ -0,0 +1,8 @@
+export type {
+  ClientOAuth2Options,
+  ClientOAuth2RequestObj
+} from "src/ClientOAuth2";
+export type { ClientOAuth2TokenData } from "src/ClientOAuth2Token";
+
+export { ClientOAuth2 } from "src/ClientOAuth2";
+export { ClientOAuth2Token } from "src/ClientOAuth2Token";
